c2a5e511d743a37398c2254cd16364951baa85c6
unknown <eyiquan811@163.com>
2020-09-19 19:48:31 +0800

    forEach循环,字符串ES6拼接

--- curriculum/js/script.js
+++ curriculum/js/script.js
@@ -4,27 +4,40 @@
 const divDom = window.Eyq.$("container-wrap");  // 获取元素
 // 固定语法
 const tableDom = window.Eyq.createEl('table'); // 创建DOM元素
-const styles = {
+
+window.Eyq.setAttr(tableDom, {
     'width': '100%',
     'border': '0',
     'cellpadding': '0',
     'cellspacing': '0',
     'id': 'table-data-wrap'
-}
-window.Eyq.setAttr(tableDom, styles)
+})
 
+// array
+const tableTheadData = [
+    { label: "头像" },
+    { label: "姓名" },
+    { label: "性别" },
+    { label: "年龄" },
+    { label: "手机号" },
+    { label: "国籍" },
+    { label: "爱好", width: 200, id: "aaa" },
+    { label: "头衔" },
+    { label: "操作" }
+]
 
-tableDom.innerHTML = `<thead>
-                        <tr>
-                            <th>头像</th>
-                            <th>姓名</th>
-                            <th>性别</th>
-                            <th>年龄</th>
-                            <th>手机号</th>
-                            <th>国籍</th>
-                            <th width="200">爱好</th>
-                            <th>头衔</th>
-                            <th>操作</th>
-                        </tr>
-                    </thead>`
+let theadHtml = `<thead><tr>`;
+// es6 forEach
+tableTheadData.forEach((item) => {
+    // 检测是否有width
+    let width = item.width ? ` width="${item.width}"` : "";
+    // 检测是否有id
+    let id = item.id ? ` id="${item.id}"` : "";
+    // 字符串拼接
+    theadHtml += `<th${width}${id}>${item.label}</th$>`;
+})
+
+theadHtml += `</tr></thead>`;
+// 生成表头
+tableDom.innerHTML = theadHtml
 divDom.appendChild(tableDom)
